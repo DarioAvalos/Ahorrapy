7923b1e6a678c4ef77c0162315dbe3dd26f5e42e
Dario8517 <darioavalos8517@gmail.com>
2024-03-11 21:52:08 -0300

    Se inicia app de tesis

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ahorrapy',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
